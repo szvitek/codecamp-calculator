53bb1bae049f629506d145801de2893bb35c8092
szvitek <gy.szvitek@gmail.com>
2023-11-29 19:37:23 +0000

    feat(Calculator): make the app work, finish user stories, update readme

--- README.md
+++ README.md
@@ -4,6 +4,15 @@ About
 
 This project is part of freeCodeCamp's frontend libraries curriculum. Objective: Build an app that is functionally similar to this: https://javascript-calculator.freecodecamp.rocks/.
 
+This calcluator just looks like on my phone, except the app only evaluates the formula uppon the equal button has pressed to make the codecamp tests pass. The percentage calculation might not be perfect, howerver I always get the same results as on my phone
+
+## screenshots
+
+| phone | app |
+| -- | -- |
+| ![](screens/1.jpg) | ![](screens/3.png) |
+| ![](screens/2.jpg) | ![](screens/4.png) |
+
 ## User stories
 
 - [x] User Story #1: My calculator should contain a clickable element containing an = (equal sign) with a corresponding id="equals".
@@ -18,20 +27,20 @@ This project is part of freeCodeCamp's frontend libraries curriculum. Objective:
 
 - [x] User Story #6: My calculator should contain an element to display values with a corresponding id="display".
 
-- [] User Story #7: At any time, pressing the clear button clears the input and output values, and returns the calculator to its initialized state; 0 should be shown in the element with the id of display.
+- [x] User Story #7: At any time, pressing the clear button clears the input and output values, and returns the calculator to its initialized state; 0 should be shown in the element with the id of display.
 
-- [] User Story #8: As I input numbers, I should be able to see my input in the element with the id of display.
+- [x] User Story #8: As I input numbers, I should be able to see my input in the element with the id of display.
 
-- [] User Story #9: In any order, I should be able to add, subtract, multiply and divide a chain of numbers of any length, and when I hit =, the correct result should be shown in the element with the id of display.
+- [x] User Story #9: In any order, I should be able to add, subtract, multiply and divide a chain of numbers of any length, and when I hit =, the correct result should be shown in the element with the id of display.
 
-- [] User Story #10: When inputting numbers, my calculator should not allow a number to begin with multiple zeros.
+- [x] User Story #10: When inputting numbers, my calculator should not allow a number to begin with multiple zeros.
 
-- [] User Story #11: When the decimal element is clicked, a . should append to the currently displayed value; two . in one number should not be accepted.
+- [x] User Story #11: When the decimal element is clicked, a . should append to the currently displayed value; two . in one number should not be accepted.
 
-- [] User Story #12: I should be able to perform any operation (+, -, \*, /) on numbers containing decimal points.
+- [x] User Story #12: I should be able to perform any operation (+, -, \*, /) on numbers containing decimal points.
 
-- [] User Story #13: If 2 or more operators are entered consecutively, the operation performed should be the last operator entered (excluding the negative (-) sign). For example, if 5 + _ 7 = is entered, the result should be 35 (i.e. 5 _ 7); if 5 _ - 5 = is entered, the result should be -25 (i.e. 5 _ (-5)).
+- [x] User Story #13: If 2 or more operators are entered consecutively, the operation performed should be the last operator entered (excluding the negative (-) sign). For example, if 5 + _ 7 = is entered, the result should be 35 (i.e. 5 _ 7); if 5 _ - 5 = is entered, the result should be -25 (i.e. 5 _ (-5)).
 
-- [] User Story #14: Pressing an operator immediately following = should start a new calculation that operates on the result of the previous evaluation.
+- [x] User Story #14: Pressing an operator immediately following = should start a new calculation that operates on the result of the previous evaluation.
 
-- [] User Story #15: My calculator should have several decimal places of precision when it comes to rounding (note that there is no exact standard, but you should be able to handle calculations like 2 / 7 with reasonable precision to at least 4 decimal places).
+- [x] User Story #15: My calculator should have several decimal places of precision when it comes to rounding (note that there is no exact standard, but you should be able to handle calculations like 2 / 7 with reasonable precision to at least 4 decimal places).
--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,148 @@
 <script setup lang="ts">
-const buttons = [
+import { ref } from "vue";
+
+type Button = {
+  type: string;
+  value: string;
+  id: string;
+};
+
+const buttons: Button[] = [
   { type: "action", value: "AC", id: "clear" },
   { type: "action", value: "&lArr;", id: "back" },
-  { type: "action", value: "%", id: "percent" },
-  { type: "action", value: "/", id: "divide" },
+  { type: "operation", value: "%", id: "percent" },
+  { type: "operation", value: "/", id: "divide" },
   { type: "number", value: "7", id: "seven" },
   { type: "number", value: "8", id: "eight" },
   { type: "number", value: "9", id: "nine" },
-  { type: "action", value: "x", id: "multiply" },
+  { type: "operation", value: "*", id: "multiply" },
   { type: "number", value: "4", id: "four" },
   { type: "number", value: "5", id: "five" },
   { type: "number", value: "6", id: "six" },
-  { type: "action", value: "-", id: "subtract" },
+  { type: "operation", value: "-", id: "subtract" },
   { type: "number", value: "1", id: "one" },
   { type: "number", value: "2", id: "two" },
   { type: "number", value: "3", id: "three" },
-  { type: "action", value: "+", id: "add" },
+  { type: "operation", value: "+", id: "add" },
   { type: "number", value: "0", id: "zero" },
   { type: "decimal", value: ".", id: "decimal" },
   { type: "action", value: "=", id: "equals" },
 ];
+
+const formula = ref("");
+const output = ref("");
+const evaluated = ref(false);
+const currentOperand = ref("");
+
+function normalizeFormula(formula: string): string {
+  if (!formula.includes("%")) {
+    return eval(formula);
+  }
+
+  const regex = /[\d\+\-\/\*\.]*%/g;
+  const matches = formula.match(regex);
+  // get percentage 20+40-30% => -30
+  const percentage = matches![0].slice(0, -1);
+  const percentageNumber = percentage.match(/[\d\.]+$/);
+
+  // get operands before the % operand 20+40-30% => 20+40
+  const operand = matches![0].replace(/[\/\*\-\+\.]*[\d]+%$/, "") || "1";
+
+  // get operator 20+40-30% => -
+  const operatorsArr = matches![0].match(/[\/\*\-\+]+/g);
+  const [operator, isNegative] = operatorsArr
+    ? operatorsArr[operatorsArr.length - 1]
+    : "*";
+
+  const percentageValue =
+    (eval("/*".includes(operator) ? "1" : operand) / 100) *
+    Number(percentageNumber![0]);
+
+  const newFormula = formula.replace(
+    matches![0],
+    eval(operand + operator + `${isNegative ?? ""}${percentageValue}`),
+  );
+  return normalizeFormula(newFormula);
+}
+
+const handleClick = (button: Button) => {
+  if (evaluated.value) {
+    formula.value = output.value;
+    currentOperand.value = "";
+  }
+
+  evaluated.value = false;
+
+  // clear
+  if (button.id === "clear") {
+    formula.value = "";
+    output.value = "";
+    currentOperand.value = "";
+  }
+
+  // back
+  if (button.id === "back") {
+    formula.value = formula.value.slice(0, -1);
+    currentOperand.value = currentOperand.value.slice(0, -1);
+  }
+
+  // evaluation
+  if (button.id === "equals") {
+    evaluated.value = true;
+
+    // count % value
+
+    const normalizedFormula = normalizeFormula(formula.value);
+    output.value = `${
+      Math.round(eval(normalizedFormula) * 10000) / 10000 || 0
+    }`;
+    currentOperand.value = "";
+  }
+
+  // input number / decimal
+  if (button.type === "number" || button.type === "decimal") {
+    const inputVal =
+      !currentOperand.value.length && button.value === "."
+        ? "0."
+        : button.value;
+
+    // check multiple 0
+    if (formula.value === "0" && inputVal === "0") {
+      return;
+    }
+
+    // check multiple .
+    if (currentOperand.value.includes(".") && button.value === ".") {
+      return;
+    }
+
+    currentOperand.value = `${currentOperand.value}${inputVal}`;
+    formula.value = `${formula.value}${inputVal}`;
+    output.value = `${currentOperand.value}`;
+  }
+
+  if (button.type === "operation") {
+    currentOperand.value = "";
+
+    if (!formula.value.length && button.value !== "-") {
+      return;
+    }
+
+    let newFormula = `${formula.value}${button.value}`;
+    if (
+      (button.value === "-" &&
+        "/*%".includes(formula.value[formula.value.length - 1])) ||
+      (button.value === "+" &&
+        "%".includes(formula.value[formula.value.length - 1]))
+    ) {
+      newFormula = `${formula.value}${button.value}`;
+    } else {
+      newFormula = newFormula.replace(/[\/\*\-\+\%]{2,3}$/, button.value);
+    }
+
+    formula.value = newFormula;
+  }
+};
 </script>
 
 <template>
@@ -29,10 +152,20 @@ const buttons = [
   >
     <div
       id="screen"
-      class="flex min-h-[126px] flex-col items-end justify-end bg-gray-500 p-2"
+      class="flex min-h-[126px] flex-col items-end justify-end rounded-lg bg-gray-500 p-2"
     >
-      <div id="formula" class="">2+3</div>
-      <div id="display">5</div>
+      <div
+        id="formula"
+        :class="[!evaluated ? 'text-2xl text-white' : 'text-sm text-gray-300']"
+      >
+        {{ formula || "0" }}
+      </div>
+      <div
+        id="display"
+        :class="[evaluated ? 'text-2xl text-white' : 'text-sm text-gray-300']"
+      >
+        {{ output || 0 }}
+      </div>
     </div>
     <div class="buttons grid grid-cols-4 gap-4 p-2">
       <button
@@ -40,6 +173,7 @@ const buttons = [
         :key="b.value"
         :id="b.id"
         class="h-12 w-12 rounded-full"
+        @click="handleClick(b)"
       >
         <p
           class="h-12 w-12 rounded-full bg-slate-300 bg-opacity-0 leading-[3rem] transition-colors duration-500 hover:bg-opacity-20"
@@ -51,12 +185,6 @@ const buttons = [
 </template>
 
 <style scoped>
-/* 
-break-normal	overflow-wrap: normal; word-break: normal;
-break-words	overflow-wrap: break-word;
-break-all	word-break: break-all;
-break-keep	word-break: keep-all;
-*/
 #formula {
   @apply break-all text-right;
 }
